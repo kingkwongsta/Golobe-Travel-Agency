c1ac9594660d64a5288ec6804f031114fc581723
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-21 21:15:18 -0700

    daisy ui configs and testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +15,10 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="btn">button here</button>
+        <button className="btn btn-primary text-2xl rounded-full">
+          Button
+        </button>
       </main>
     </>
   );
