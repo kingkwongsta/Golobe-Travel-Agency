c2e52373a7f18aacbe3f73d5e2a33109961bf393
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-21 21:57:53 -0700

    text overlay on top of input fields

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -4,30 +4,37 @@ export default function SearchBar() {
   return (
     <>
       <div className="text-xl">Where are you flying?</div>
-      <div className="input-section flex">
-        <div className="relative">
-          <p className="absolute top-0">hello there</p>
+      <div className="input-section flex mt-[32px]">
+        <div className="from-input relative">
+          <p className="absolute top-[-12px] left-[10px]">
+            <span className="bg-white pl-2 pr-1.5">From - To</span>
+          </p>
+          <input
+            type="text"
+            placeholder="Los Angeles - Taipei"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div className="from-input relative">
+          <p className="absolute top-[-12px] left-[10px]">
+            <span className="bg-white pl-2 pr-1.5">Depart - Return</span>
+          </p>
           <input
             type="text"
             placeholder="Type here"
             className="input input-bordered w-full max-w-xs"
           />
         </div>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
+        <div className="from-input relative">
+          <p className="absolute top-[-12px] left-[10px]">
+            <span className="bg-white pl-2 pr-1.5">Passenger - Class</span>
+          </p>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
       </div>
     </>
   );
