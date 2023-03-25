c2b4e5c517df7d6c49b50fd7eab92716f60edc43
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-24 22:16:47 -0700

    top section of discover

--- src/components/discover/discover.jsx
+++ src/components/discover/discover.jsx
@@ -1,10 +1,15 @@
 export default function Discovery() {
   return (
-    <>
-      <div>
-        <h2>Plan your perfect trip</h2>
-        <p>Search Flights & Places Hire to our most popular destinations</p>
+    <div className="mx-[32px]">
+      <div className="flex flex-row justify-between content-center">
+        <div>
+          <h2 className="mb-[16px] text-3xl">Plan your perfect trip</h2>
+          <p className="text-lg">
+            Search Flights & Places Hire to our most popular destinations
+          </p>
+        </div>
+        <button className="btn btn-outline">See more places</button>
       </div>
-    </>
+    </div>
   );
 }
