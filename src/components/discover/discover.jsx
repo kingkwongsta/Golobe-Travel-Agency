010cfc4f730430c41488c63df2c9128fa81b6797
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-24 22:10:53 -0700

    base file of discovery component

---
+++ src/components/discover/discover.jsx
@@ -0,0 +1,10 @@
+export default function Discovery() {
+  return (
+    <>
+      <div>
+        <h2>Plan your perfect trip</h2>
+        <p>Search Flights & Places Hire to our most popular destinations</p>
+      </div>
+    </>
+  );
+}
