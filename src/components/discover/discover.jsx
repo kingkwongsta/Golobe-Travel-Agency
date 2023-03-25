56759d220d5588f9ce02a9a6f84bb1c39616ce86
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-24 22:27:45 -0700

    base styling for citie elements

--- src/components/discover/discover.jsx
+++ src/components/discover/discover.jsx
@@ -18,7 +18,7 @@ export default function Discovery() {
   }
   return (
     <div className="mx-[32px]">
-      <div className="flex flex-row justify-between content-center">
+      <div className="flex flex-row justify-between content-center mb-[40px]">
         <div>
           <h2 className="mb-[16px] text-3xl">Plan your perfect trip</h2>
           <p className="text-lg">
@@ -27,7 +27,7 @@ export default function Discovery() {
         </div>
         <button className="btn btn-outline">See more places</button>
       </div>
-      <div className="grid grid-cols-3">{renderCityElements()}</div>
+      <div className="grid grid-cols-3 gap-[32px]">{renderCityElements()}</div>
     </div>
   );
 }
