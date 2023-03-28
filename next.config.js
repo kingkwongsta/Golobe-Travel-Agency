39799dfdf0af85d39a89a1e3db2128f336559d3f
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-03-27 22:58:43 -0700

    added unsplash to image whitelist

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
