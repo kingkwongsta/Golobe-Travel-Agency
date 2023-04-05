1de11b68d9bd1bf0002783a41d6132119720d5ef
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-04 21:01:25 -0700

    setup review component

---
+++ src/components/reviews/reviews.jsx
@@ -0,0 +1,48 @@
+export default function Reviews() {
+  return (
+    <div className="carousel rounded-box w-96">
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1494253109108-2e30c049369b.jpg"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1559181567-c3190ca9959b.jpg"
+          className="w-full"
+        />
+      </div>
+      <div className="carousel-item w-1/2">
+        <img
+          src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+          className="w-full"
+        />
+      </div>
+    </div>
+  );
+}
