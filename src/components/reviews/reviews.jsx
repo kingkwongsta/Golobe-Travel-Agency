2c2d33c83349d1fc1cfd5fd88b54fd92956896ed
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-04 21:10:33 -0700

    setup boilerplate for carousel

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -3,17 +3,26 @@ import placeholder from "./../../../public/travel-placeholder.jpg";
 
 export default function Reviews() {
   return (
-    <div className="carousel rounded-box w-96">
-      <div className="carousel-item w-1/2">
+    <div className="carousel carousel-center max-w-[2000px] mx-[80px] p-4 space-x-[48px] rounded-box">
+      <div className="carousel-item">
         <Image src={placeholder} width={400} height={800} alt="text" />
       </div>
-      <div className="carousel-item w-1/2">
+      <div className="carousel-item">
         <Image src={placeholder} width={400} height={800} alt="text" />
       </div>
-      <div className="carousel-item w-1/2">
+      <div className="carousel-item">
         <Image src={placeholder} width={400} height={800} alt="text" />
       </div>
-      <div className="carousel-item w-1/2">
+      <div className="carousel-item">
+        <Image src={placeholder} width={400} height={800} alt="text" />
+      </div>
+      <div className="carousel-item">
+        <Image src={placeholder} width={400} height={800} alt="text" />
+      </div>
+      <div className="carousel-item">
+        <Image src={placeholder} width={400} height={800} alt="text" />
+      </div>
+      <div className="carousel-item">
         <Image src={placeholder} width={400} height={800} alt="text" />
       </div>
     </div>
