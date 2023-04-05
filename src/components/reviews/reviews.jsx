3e5a7642a8bb3a0c894bd58ea6212e284247a036
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-04 21:13:43 -0700

    top text for reviews

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -3,27 +3,33 @@ import placeholder from "./../../../public/travel-placeholder.jpg";
 
 export default function Reviews() {
   return (
-    <div className="carousel carousel-center max-w-[2000px] mx-[80px] p-4 space-x-[48px] rounded-box">
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
+    <div className="mx-[80px]">
+      <div className="mb-[16px]">
+        <div className="text-3xl">Reviews</div>
+        <p className="text-lg">What people says about Golobe facilities</p>
       </div>
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
-      </div>
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
-      </div>
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
-      </div>
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
-      </div>
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
-      </div>
-      <div className="carousel-item">
-        <Image src={placeholder} width={400} height={800} alt="text" />
+      <div className="carousel carousel-center max-w-[2000px] p-4 space-x-[48px] rounded-box">
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
+        <div className="carousel-item">
+          <Image src={placeholder} width={400} height={800} alt="text" />
+        </div>
       </div>
     </div>
   );
