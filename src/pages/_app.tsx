733646ac5703db1461e4d02b4900a0390b291b2a
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-05 23:15:06 -0700

    configure global font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
+
+// const gothic = localFont({
+//   src: [
+//     {
+//       path: "../../public/font/Trade Gothic/Trade Gothic LT.ttf",
+//       weight: "500",
+//     },
+//     {
+//       path: "../../public/font/Trade Gothic/Trade Gothic LT.ttf",
+//       weight: "700",
+//     },
+//   ],
+//   variable: "--font-gothic",
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
