33060000bb36afbc378717bf19a7089c3b40e01e
kwongster <bryant.j.kwong@gmail.com>
2023-04-06 21:31:49 -0700

    flex row of two sides, left and right setup

--- src/components/subscribe/subscribe.jsx
+++ src/components/subscribe/subscribe.jsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import mail from "./../../../public/img/mailbox.png";
+
 export default function Subscribe() {
   return (
-    <div>
-      <div className="flex flex-col">
+    <div className="flex flex-row">
+      {/* LEFT SIDE */}
+      <div className="flex flex-col basis-[808px]">
         <p>Subscribe</p>
         <p>Newsletter</p>
         <p>The Travel</p>
@@ -15,10 +19,12 @@ export default function Subscribe() {
             placeholder="Your email address"
             className="input w-full max-w-[473px] max-h-[56px]"
           />
-
           <button className="btn">Subscribe</button>
         </div>
-        <div></div>
+      </div>
+      {/* RIGHT SIDE */}
+      <div>
+        <Image src={mail} width={400} height={305} alt="mailbox" />
       </div>
     </div>
   );
