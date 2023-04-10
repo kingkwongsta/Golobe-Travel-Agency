bf8a1236f8006c03c45057607fa8c7ebb08a303d
kwongster <bryant.j.kwong@gmail.com>
2023-04-09 22:43:20 -0700

    font settings for heading of discover component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Roboto } from "@next/font/google";
+import { Montserrat } from "@next/font/google";
 
-const roboto = Roboto({
+const montserrat = Montserrat({
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     // <div className="bg-[#FAFAFA]">
-    <div className={roboto.className}>
-      <Component {...pageProps} />
+    <div className={montserrat.className}>
+      <div className="bg-[#FAFAFA]">
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
