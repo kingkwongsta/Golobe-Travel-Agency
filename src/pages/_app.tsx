d6ec6fd10064d22f87cf6a974f1c8f4bcde0face
kwongster <bryant.j.kwong@gmail.com>
2023-04-09 22:37:30 -0700

    setup roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-[#FAFAFA]">
+    // <div className="bg-[#FAFAFA]">
+    <div className={roboto.className}>
       <Component {...pageProps} />
     </div>
   );
