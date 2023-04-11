44faa8dd2bd095c33a9084e6f1edc9c31d1c50fb
kwongster <bryant.j.kwong@gmail.com>
2023-04-10 20:39:23 -0700

    fix image sizing to fit container

--- src/components/search/searchbar.jsx
+++ src/components/search/searchbar.jsx
@@ -7,9 +7,9 @@ import cover from "./../../../public/landing-page-searchbar.avif";
 export default function SearchBar() {
   return (
     <div className="mt-[40px] mb-[180px] mx-[80px] max-w-[1380px]">
-      <div className="relative max-w-[1380px] max-h-[581px] flex justify-center">
+      <div className="relative max-w-[1380px] max-h-[581px] flex justify-center lg:min-w-[1380px]">
         <Image
-          className="rounded-[24px] flex-shrink-0 min-w-full max-w-full"
+          className="rounded-[24px] flex-shrink-0 min-w-full max-w-full object-cover"
           src={cover}
           width={1380}
           height={581}
