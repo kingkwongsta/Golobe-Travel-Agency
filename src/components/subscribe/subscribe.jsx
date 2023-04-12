7abe3f9e927ba497ef5c1a5264f2a8729cd873d6
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-11 20:21:59 -0700

    font settings for subscribe component

--- src/components/subscribe/subscribe.jsx
+++ src/components/subscribe/subscribe.jsx
@@ -6,16 +6,16 @@ export default function Subscribe() {
     <div className="flex flex-row bg-[#CDEAE1] my-[80px] mx-[80px] max-w-[1232px] rounded-[20px]">
       {/* LEFT SIDE */}
       <div className="flex flex-col basis-[808px] mt-[24px] ml-[24px]">
-        <p className="text-[44px] font-[700] leading-[54px] text-[#112211]">
+        <p className="text-[44px] font-[600] leading-[54px] text-[#112211]">
           Subscribe
         </p>
-        <p className="mb-[24px] text-[44px] font-[700] leading-[54px] text-[#112211]">
+        <p className="mb-[24px] text-[44px] font-[600] leading-[54px] text-[#112211]">
           Newsletter
         </p>
-        <p className="mb-[8px] text-[700] text-[20px] leading-[25px] text-[#112211]">
+        <p className="mb-[8px]  opacity-[.8] font-[700] text-[20px] leading-[25px] text-[#112211]">
           The Travel
         </p>
-        <p className="mb-[16px] text-[500] text-[16px] leading-[20px] text-[#112211]">
+        <p className="mb-[16px] opacity-[.7] font-[500] text-[16px] leading-[20px] text-[#112211]">
           Get inspired! Receive travel discounts, tips and behind the scenes
           stories.
         </p>
@@ -25,7 +25,7 @@ export default function Subscribe() {
             placeholder="Your email address"
             className="mr-[16px] input w-full max-w-[473px] max-h-[56px] text-[400] text-[16px] leading-[20px] text-[#1C1B1F]"
           />
-          <button className="btn text-[600] text-[14px] leading-[17px] text-[#FFFFFF]">
+          <button className="btn font-[600] text-[14px] leading-[17px] text-[#FFFFFF] normal-case bg-[#112211]">
             Subscribe
           </button>
         </div>
