a235fd2feb95d034058828b3dcd7cd1a185b9856
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-13 13:52:21 -0700

    button and font settings to top section of review component

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -6,17 +6,24 @@ import ReviewCard from "./reviewcard";
 
 export default function Reviews() {
   return (
-    <div className="mx-[80px]">
+    <div className="max-w-[1400px]">
+      {/* TOP SECTION */}
       <div className="mb-[16px]">
-        <div className="text-3xl">Reviews</div>
-        <p className="text-lg">What people says about Golobe facilities</p>
+        <div className="font-[600] text-[32px]">Reviews</div>
+        <p className="font-[400] text-[16px]">
+          What people says about Golobe facilities
+        </p>
+        <button className="btn bg-transparent border-[#8DD3BB] rounded-[4px] font-[500] text-[14px] text-[#112211]">
+          See All
+        </button>
       </div>
+      {/* <----------------> */}
       <div className="carousel p-4 space-x-[48px] rounded-box">
         <ReviewCard className="carousel-item" />
         <ReviewCard className="carousel-item" />
         <ReviewCard className="carousel-item" />
-        <ReviewCard className="carousel-item" />
-        <ReviewCard className="carousel-item" />
+        {/* <ReviewCard className="carousel-item" />
+        <ReviewCard className="carousel-item" /> */}
       </div>
     </div>
   );
