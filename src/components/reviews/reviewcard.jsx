a692ba57aa743512ef1d9664366bcf87eeeb72fa
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-13 14:16:53 -0700

    remainder of hardcoded elements replaced with data import

--- src/components/reviews/reviewcard.jsx
+++ src/components/reviews/reviewcard.jsx
@@ -21,13 +21,8 @@ export default function ReviewCard({ tagline, review, star, name, source }) {
       <div className="w-[450px] h-[608px]">
         <div>
           <div className="w-[425px] h-[584px] flex flex-col shadow-md shadow-[#1122111a] rounded-[20px] bg-white absolute p-[24px] z-50">
-            <p className="text-[24px] font-[700] mb-[30px]">
-              “A real sense of community, nurtured”
-            </p>
-            <p className="text-[14px] font-[500] mb-[12px]">
-              Really appreciate the help and support from the staff during these
-              tough times. Shoutout to Katie for...
-            </p>
+            <p className="text-[24px] font-[700] mb-[30px]">{tagline}</p>
+            <p className="text-[14px] font-[500] mb-[12px]">{review}</p>
             <p className="text-[14px] font-[700] mb-[16px] text-right">
               View more
             </p>
@@ -36,8 +31,8 @@ export default function ReviewCard({ tagline, review, star, name, source }) {
               {renderStarElements()}
             </div>
             <div className="review-person mb-[12px]">
-              <p className="text-[14px] font-[700]">Olga</p>
-              <p className="text-[12px] font-[500]">Weave Studios – Kai Tak</p>
+              <p className="text-[14px] font-[700]">{name}</p>
+              <p className="text-[12px] font-[500]">{source}</p>
             </div>
             <div className="review-source flex flex-row mb-[40px]">
               <Image src={google} height={24} width={24} alt="google" />
