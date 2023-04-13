13f336f01fb4aaefbdc094da7f3091dbd5fb2e66
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-13 13:40:20 -0700

    search bar font and padding cleanup

--- src/components/search/searchbar.jsx
+++ src/components/search/searchbar.jsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import airplane from "./../../../public/airplane.png";
 import stays from "./../../../public/bed.png";
 import cover from "./../../../public/landing-page-searchbar.avif";
+import send from "./../../../public/img/send.png";
 
 export default function SearchBar() {
   return (
-    <div className="mt-[40px] mb-[180px] mx-[80px]">
+    <div className="mt-[25px] mb-[140px] mx-[80px]">
       <div className="relative lg:min-w-[1380px] max-h-[581px] flex justify-center">
         <Image
           className="rounded-[24px] flex-shrink-0 min-w-full max-w-full object-cover"
@@ -32,17 +33,35 @@ export default function SearchBar() {
           {/* SEARCH COMPONENTS */}
           <div className="m-[24px]">
             {/*-------- FLIGHTS OR STAY --------*/}
-            <div className="top-icons flex flex-row items-center gap-6 my-6">
+            <div className="top-icons flex flex-row items-center gap-6">
               <div className="flex flex-row">
-                <Image src={airplane} height={24} width={24} alt="airplane" />
-                <span>Flights</span>
+                <Image
+                  className="mr-[9px]"
+                  src={airplane}
+                  height={24}
+                  width={24}
+                  alt="airplane"
+                />
+                <span className="font-[600] text-[16px] text-[#112211]">
+                  Flights
+                </span>
               </div>
-              <div className="vertical-line border-l-[1px] h-8"></div>
+              <div className="vertical-line border-l-[1px] h-8 mx-[16px] border-[#D7E2EE]"></div>
               <div className="flex flex-row">
-                <Image src={stays} height={24} width={24} alt="airplane" />
-                <span>Stays</span>
+                <Image
+                  className="mr-[9px]"
+                  src={stays}
+                  height={24}
+                  width={24}
+                  alt="airplane"
+                />
+                <span className="font-[600] text-[16px] text-[#112211]">
+                  Stays
+                </span>
               </div>
             </div>
+            {/* Horizontal Line */}
+            <hr className="max-w-[110px] mt-[15px] mb-[35px] border-[2px] border-[#8DD3BB]"></hr>
             {/*-------- TRAVEL INPUT --------*/}
             <div className="input-section flex gap-8 my-6">
               <div className="from-input relative flex-1">
@@ -88,6 +107,13 @@ export default function SearchBar() {
                 + Add Promo Code
               </p>
               <button className="btn btn-primary font-[500] text-[14px] text-[#112211] max-h-[48px] capitalize bg-[#8DD3BB]">
+                <Image
+                  className="mr-[5px]"
+                  src={send}
+                  width={17}
+                  height={17}
+                  alt="send"
+                />
                 Show Flights
               </button>
             </div>
